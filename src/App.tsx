3b1eb506425bba7afd0e97bce606c6b8fd8d8e8f
leejieun <eunsa3220@naver.com>
2026-05-18 11:17:48 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SmsInviteB } from './components/SmsInviteB';
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <SmsInviteB />
+    </div>
+  );
+}
+
+export default App;
